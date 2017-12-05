b99cfd840a91022cacd90219736b6ede5bb8b029
h-youhei <hukumitu.youhei@gmail.com>
2017-12-05 20:56:06 +0900

    move to layouts directory. (qmk/qmk_firmware#1609)

--- layouts/community/ergodox/youhei/keymap.c
+++ layouts/community/ergodox/youhei/keymap.c
@@ -3,7 +3,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[L_BASE] = KEYMAP(
 		// left hand
-		KC_SLCK, KC_7, KC_8, KC_9, KC_0, KC_6, KC_GRV,
+		KC_CAPS, KC_7, KC_8, KC_9, KC_0, KC_6, KC_GRV,
 		KC_LBRC, KC_X, KC_C, KC_L, KC_P, KC_V, KC_BSPC,
 		KC_QUOT, KC_O, KC_S, KC_R, KC_N, KC_H,
 		KC_EQL, KC_Q, KC_J, KC_COMM, KC_F, KC_B, KC_ESC,
@@ -11,9 +11,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		// thumb
 		KC_NO, KC_NO,
 		KC_NO,
-		LSFT_T(KC_SPC), FN_T(KC_ENT), KC_CAPS,
+		LSFT_T(KC_SPC), FN_T(KC_ENT), KC_MHEN,
 		// right hand
-		KC_INS, KC_5, KC_1, KC_2, KC_3, KC_4, KC_KANA,
+		KC_INS, KC_5, KC_1, KC_2, KC_3, KC_4, KC_CAPS,
 		KC_DEL, KC_K, KC_G, KC_U, KC_Y, KC_Z, KC_RBRC,
 		KC_D, KC_T, KC_E, KC_I, KC_A, KC_SCLN,
 		KC_TAB, KC_W, KC_M, KC_DOT, KC_MINS, KC_SLSH, KC_BSLS,
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		// thumb
 		KC_NO, KC_NO,
 		KC_NO,
-		KC_MHEN, FN_T(KC_HENK), RSFT_T(KC_SPC)
+		KC_HENK, FN_T(KC_KANA), RSFT_T(KC_SPC)
 	),
 	[L_FN] = KEYMAP(
 		// left hand
